799814a179c4be79932da98bcd47107982b409c5
Aurélien Lheureux <dev@aurelienlheureux.com>
2020-06-02 17:14:43 +0200

    Jour 1

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('Je suis un script javascript');
+
+/*var nom = prompt('Entrez votre nom');
+
+console.log(nom);
+
+if(nom == 'Aurélien') {
+    console.log('Vous êtes connectés');
+    window.location = './mon-compte.html';
+}else{
+    console.log('Nom incorrect');
+    window.location = './erreur.html';
+}*/
